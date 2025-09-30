dbf34a7e623868eef8270c280555c46b18e30cd3
chiragbansaldinesh-boop <chiragbansaldinesh@gmail.com>
2025-09-30 19:50:55 +0530

    Updated discover.tsx

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -1,27 +1,25 @@
-@@ .. @@
- import { DEFAULT_CAFE } from '@/utils/locationUtils';
-+import { useUser, useVenueUsers, useLocationUpdates } from '@/hooks/useFirestore';
-+import { firestoreService } from '@/services/firestoreService';
-+import { geoPointToCoordinates } from '@/utils/firestoreHelpers';
+import { DEFAULT_CAFE } from '@/utils/locationUtils';
+import { useUser, useVenueUsers, useLocationUpdates } from '@/hooks/useFirestore';
+import { firestoreService } from '@/services/firestoreService';
+import { geoPointToCoordinates } from '@/utils/firestoreHelpers';
 
-@@ .. @@
- export default function DiscoverScreen() {
-+  // TODO: Replace with actual user ID from authentication
-+  const currentUserId = 'current-user-id';
-+  
-+  const { user: currentUser, updateUser } = useUser(currentUserId);
-+  const { users: venueUsers, loading: venueUsersLoading } = useVenueUsers('venue-001');
-+  const { updateLocation } = useLocationUpdates(currentUserId);
-+  
-   const [users, setUsers] = useState(mockUsers);
-@@ .. @@
-   
-   React.useEffect(() => {
-     loadBlockedUsers();
-+    
-+    // TODO: Update user location in Firestore when location changes
-+    if (isWithinCafe && currentUser?.location) {
-+      const coords = geoPointToCoordinates(currentUser.location);
-+      updateLocation(coords.latitude, coords.longitude);
-+    }
-   }, []);
+export default function DiscoverScreen() {
+  // TODO: Replace with actual user ID from authentication
+  const currentUserId = 'current-user-id';
+  
+  const { user: currentUser, updateUser } = useUser(currentUserId);
+  const { users: venueUsers, loading: venueUsersLoading } = useVenueUsers('venue-001');
+  const { updateLocation } = useLocationUpdates(currentUserId);
+  
+  const [users, setUsers] = useState(mockUsers);
+  
+  React.useEffect(() => {
+    loadBlockedUsers();
+    
+    // TODO: Update user location in Firestore when location changes
+    if (isWithinCafe && currentUser?.location) {
+      const coords = geoPointToCoordinates(currentUser.location);
+      updateLocation(coords.latitude, coords.longitude);
+    }
+  }, []);
+}
